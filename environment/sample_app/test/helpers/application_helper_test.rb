34a7915624f5b95e6bc856a850dd6169463d4101
Shahryar Faraji <shahryar@1receipt.io>
2019-04-03 14:31:21 +1100

    done until 5.4

---
+++ environment/sample_app/test/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+test/helpers/application_helper_test.rb
+ require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,         FILL_IN
+    assert_equal full_title("Help"), FILL_IN
+  end
+end
