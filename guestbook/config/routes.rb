1810f3b6f6c61b01f81d72e4d38aa8a7a900695f
Shahryar Faraji <shahryar@1receipt.io>
2019-04-04 12:49:15 +1100

    added the view

--- guestbook/config/routes.rb
+++ guestbook/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'entries/sign_in'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'entries/sign_in' => 'entries#sign_in'
+  post 'entries/sign_in' => 'entries#sign_in'
 end
