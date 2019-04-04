44267641dc7ca33d23b1ffde2f9a52cf30ef7c3b
Shahryar Faraji <shahryar@1receipt.io>
2019-04-04 20:56:41 +1100

    fixed the stupid mistake  in the test which cost me .5 mark

--- environment/sample_app/test/helpers/application_helper_test.rb
+++ environment/sample_app/test/helpers/application_helper_test.rb
@@ -1,9 +1,8 @@
-# test/helpers/application_helper_test.rb
- require 'test_helper'
+require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         FILL_IN
-    assert_equal full_title("Help"), FILL_IN
+    assert_equal full_title,         "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
   end
 end
